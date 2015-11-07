61b7c17b9175a91f9c5cbf57146ac1f342b312a1
Ryan Peden <ryan04@gmail.com>
2015-11-06 20:28:37 -0500

    initial commit of working v1

---
+++ HNGrabber/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace HNGrabber
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("HNGrabber")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("ryanpeden")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ HNGrabber/Program.fs
@@ -0,0 +1,55 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System.Net
+open HtmlAgilityPack
+open Fizzler.Systems.HtmlAgilityPack
+open Newtonsoft.Json;
+
+
+type HNStory(title: string, address: string) =
+    member x.Title = title
+    member x.Address = address
+
+
+let downloadFrontPage = 
+    use client = new WebClient()
+    client.DownloadString "https://news.ycombinator.com"
+
+
+let parsePage (htmlString: string) =
+    let document = HtmlDocument()
+    document.LoadHtml htmlString
+    document.DocumentNode
+
+
+let extractStories (document: HtmlNode) =
+
+    let buildStory (storyNode: HtmlNode) = 
+        (* handle the case where the element isn't formed the way
+           we expect it to be *)
+        try
+            let story = storyNode.QuerySelector(".title a")
+            let address = story.Attributes.["href"].Value
+            let title = story.InnerText
+            Some( HNStory(title, address) )
+        with
+            | :? System.DivideByZeroException -> None
+
+    document.QuerySelectorAll(".athing")
+          |> Seq.cast<HtmlNode>
+          |> Seq.choose buildStory
+          |> Seq.cache
+
+[<EntryPoint>]
+let main argv =
+    let stories =  downloadFrontPage
+                |> parsePage
+                |> extractStories
+                |> JsonConvert.SerializeObject
+    printfn "%s" stories
+    let x = System.Console.ReadLine() 
+    0 // return an integer exit code
+
+
+
+
